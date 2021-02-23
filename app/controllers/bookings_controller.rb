881a6269ddea0e11cbd8236756e7898e46dea34f
Toomas Arumae <tom.arumae@gmail.com>
2021-02-23 16:11:05 +0000

    Start create action for booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,26 @@
 class BookingsController < ApplicationController
-  def create; end
+  def create
+    @allotment = Allotment.find(params[:allotment_id])
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.allotment = @allotment
+    @booking.total_price = @allotment.price * ((@booking.end_date - @booking.start_date) / 12)
+    raise
+    if @booking.save
+      flash[:notice] = "You have successfully booked #{@allotment.name}"
+    else
+      flash[:alert] = "End date can not be before start date."
+    end
+    redirect_to allotment_path(@allotment)
+  end
 
   def destroy; end
 
   def approve; end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,13 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :allotment
   validates :start_date, :end_date, presence: true
+  validate :check_end_date_is_after_start_date
+
+  private
+
+  def check_end_date_is_after_start_date
+    return if start_date.blank? || end_date.blank?
+
+    errors.add(:start_date, "Can not be after the end date.") if start_date > end_date
+  end
 end
