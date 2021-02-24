79ced317c044b584b955659d3040bfde6477fdaf
jihye kang <jihye59kang@gmail.com>
2021-02-24 11:40:37 +0000

    cloudinary set up and add new/create method

--- app/controllers/allotments_controller.rb
+++ app/controllers/allotments_controller.rb
@@ -1,25 +1,57 @@
 class AllotmentsController < ApplicationController
+  before_action :set_allotment, only: [:show, :edit, :update, :destroy]
+
   def index
     @allotments = Allotment.all
     @search = params["search"]
     if @search.present?
       @location = @search["location"]
       @results = Allotment.where("location ILIKE ?", @location)
+    else
+      @results = Allotment.all
     end
   end
 
   def show
-    @allotment = Allotment.find(params[:id])
     @booking = Booking.new
   end
 
-  def new; end
+  def new
+    @allotment = Allotment.new
+  end
 
-  def create; end
+  def create
+    @allotment = Allotment.create(allotment_params)
+    if @allotment.save
+      redirect_to allotment_path(@allotment), notice: 'Allotment was successfully created.'
+    else
+      render :new
+    end
+  end
 
-  def destroy; end
+  def destroy
+    @allotment.destroy
+    redirect_to allotment_path(@allotment), notice: 'Allotment was successfully destroyed.'
+  end
 
-  def update; end
+  def update
+    if @allotment.update(allotment_params)
+      redirect_to allotment_path(@allotment), notice: 'Allotment was successfully updated.'
+    else
+      render :edit
+    end
+  end
 
-  def edit; end
+  def edit
+  end
+
+  private
+
+  def set_allotment
+    @allotment = Allotment.find(params[:id])
+  end
+
+  def allotment_params
+    params.require(:allotment).permit(:name, :description, :location, :price, :area_size, photos: [])
+  end
 end
--- app/models/allotment.rb
+++ app/models/allotment.rb
@@ -2,4 +2,6 @@ class Allotment < ApplicationRecord
   belongs_to :user
   has_many :bookings
   validates :name, :description, :location, :price, :area_size, presence: true
+
+  has_many_attached :photos
 end
