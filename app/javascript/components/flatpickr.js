af273979075589c2a6df173987932026c100d283
Toomas Arumae <tom.arumae@gmail.com>
2021-02-25 19:59:35 +0000

    Add confirmation modal and disable booking button when dates aren't selected

--- app/javascript/components/flatpickr.js
+++ app/javascript/components/flatpickr.js
@@ -16,19 +16,21 @@ export const init_flatpickr = () => {
 }
 
 export const totalprice = () => {
+  let start_date = document.querySelector(".datepickerstart")
   let end_date = document.querySelector(".datepickerend");
-  if (end_date) {
+  if (end_date && start_date) {
 
     end_date.addEventListener("change", function () {
       // This still needs tweaking. Currently if you choose to rent for 1.5 months for example, it will only show total price for 1 month.
+      document.querySelector(".book-button").disabled = false;
       let start_date = new Date(document.querySelector(".datepickerstart").value);
       let end_date = new Date(document.querySelector(".datepickerend").value);
       let price = parseInt(document.querySelector(".price").innerText);
       let months = (end_date.getFullYear() - start_date.getFullYear()) * 12;
       months -= start_date.getMonth();
       months += end_date.getMonth();
-      document.querySelector(".totalprice").innerHTML = `Total Price: £${months === 0 ? price : price * months}`;
+      document.querySelector(".totalprice").innerHTML = `<p>Total Price: <strong>£${months === 0 ? price : price * months}</strong></p>`;
+      document.querySelector(".modal-body").innerHTML = `<p>Total Price: <strong>£${months === 0 ? price : price * months}</strong></p> <p>Dates: ${start_date.toLocaleDateString()} - ${end_date.toLocaleDateString()}</p>`;
     });
   }
 }
-
